6b8461ebbe996eb001de9567c4a3bd7e4a125f60
Yyyin <wjzadward0227@outlook.com>
2025-04-26 16:29:15 +0800

    [NEW]新增arm功能测试与写入打印

--- src/spark_driver/arm/uArm/swiftpro/src/swiftpro_write_node.cpp
+++ src/spark_driver/arm/uArm/swiftpro/src/swiftpro_write_node.cpp
@@ -28,6 +28,10 @@ swiftpro::SwiftproState pos;
  */
 void position_write_callback(const swiftpro::position& msg)
 {
+    ROS_INFO("=== Position Control Callback Called ===");
+    ROS_INFO("Received position: x=%.2f, y=%.2f, z=%.2f, speed=%lu", 
+             msg.x, msg.y, msg.z, msg.speed);
+    
     std::string Gcode;
     std_msgs::String result;
     std::string x = std::to_string(msg.x);
@@ -36,15 +40,17 @@ void position_write_callback(const swiftpro::position& msg)
     std::string speed;
     
     if (msg.speed) {
-		speed = std::to_string(msg.speed);
+        speed = std::to_string(msg.speed);
     } else {
         speed = "1000";
     }
 
     Gcode = "G0 X" + x + " Y" + y + " Z" + z + " F" + speed + "\r\n";
-    ROS_INFO("%s", Gcode.c_str());
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
     _serial.write(Gcode.c_str());
     result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Position Control Command Completed ===\n");
 }
 
 
@@ -55,14 +61,19 @@ void position_write_callback(const swiftpro::position& msg)
  */
 void angle1st_callback(const swiftpro::angle1st& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
-	std::string m1 = std::to_string(msg.angle1st);
+    ROS_INFO("=== Motor 1 Angle Control Callback Called ===");
+    ROS_INFO("Setting Motor 1 to angle: %.2f degrees", msg.angle1st);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
+    std::string m1 = std::to_string(msg.angle1st);
 
-	Gcode = (std::string)"G2202 N0 V" + m1 + "F100\r\n";
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    Gcode = (std::string)"G2202 N0 V" + m1 + "F100\r\n";
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Motor 1 Control Command Completed ===\n");
 }
 
 
@@ -73,14 +84,19 @@ void angle1st_callback(const swiftpro::angle1st& msg)
  */
 void angle2nd_callback(const swiftpro::angle2nd& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
-	std::string m2 = std::to_string(msg.angle2nd);
+    ROS_INFO("=== Motor 2 Angle Control Callback Called ===");
+    ROS_INFO("Setting Motor 2 to angle: %.2f degrees", msg.angle2nd);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
+    std::string m2 = std::to_string(msg.angle2nd);
 
-	Gcode = (std::string)"G2202 N1 V" + m2 + "F100\r\n";
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    Gcode = (std::string)"G2202 N1 V" + m2 + "F100\r\n";
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Motor 2 Control Command Completed ===\n");
 }
 
 
@@ -91,14 +107,19 @@ void angle2nd_callback(const swiftpro::angle2nd& msg)
  */
 void angle3rd_callback(const swiftpro::angle3rd& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
-	std::string m3 = std::to_string(msg.angle3rd);
+    ROS_INFO("=== Motor 3 Angle Control Callback Called ===");
+    ROS_INFO("Setting Motor 3 to angle: %.2f degrees", msg.angle3rd);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
+    std::string m3 = std::to_string(msg.angle3rd);
 
-	Gcode = (std::string)"G2202 N2 V" + m3 + "F100\r\n";
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    Gcode = (std::string)"G2202 N2 V" + m3 + "F100\r\n";
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Motor 3 Control Command Completed ===\n");
 }
 
 
@@ -109,14 +130,19 @@ void angle3rd_callback(const swiftpro::angle3rd& msg)
  */
 void angle4th_callback(const swiftpro::angle4th& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
-	std::string m4 = std::to_string(msg.angle4th);
+    ROS_INFO("=== Motor 4 Angle Control Callback Called ===");
+    ROS_INFO("Setting Motor 4 to angle: %.2f degrees", msg.angle4th);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
+    std::string m4 = std::to_string(msg.angle4th);
 
-	Gcode = (std::string)"G2202 N3 V" + m4 + "F500\r\n";
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    Gcode = (std::string)"G2202 N3 V" + m4 + "F500\r\n";
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Motor 4 Control Command Completed ===\n");
 }
 
 
@@ -127,22 +153,30 @@ void angle4th_callback(const swiftpro::angle4th& msg)
  */
 void swiftpro_status_callback(const swiftpro::status& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
+    ROS_INFO("=== SwiftPro Status Callback Called ===");
+    ROS_INFO("Setting status to: %d (1=attach, 0=detach)", msg.status);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
 
-	if (msg.status == 1)
-		Gcode = (std::string)"M17\r\n";   // attach
-	else if (msg.status == 0)
-		Gcode = (std::string)"M2019\r\n";
-	else
-	{
-		ROS_INFO("Error:Wrong swiftpro status input");
-		return;
-	}
+    if (msg.status == 1) {
+        Gcode = (std::string)"M17\r\n";   // attach
+        ROS_INFO("Attaching motors...");
+    }
+    else if (msg.status == 0) {
+        Gcode = (std::string)"M2019\r\n";
+        ROS_INFO("Detaching motors...");
+    }
+    else {
+        ROS_ERROR("Error: Wrong swiftpro status input");
+        return;
+    }
 
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== SwiftPro Status Command Completed ===\n");
 }
 
 
@@ -153,22 +187,30 @@ void swiftpro_status_callback(const swiftpro::status& msg)
  */
 void gripper_callback(const swiftpro::status& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
+    ROS_INFO("=== Gripper Control Callback Called ===");
+    ROS_INFO("Setting gripper to: %d (1=close, 0=open)", msg.status);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
 
-	if (msg.status == 1)
-		Gcode = (std::string)"M2232 V1" + "\r\n";
-	else if (msg.status == 0)
-		Gcode = (std::string)"M2232 V0" + "\r\n";
-	else
-	{
-		ROS_INFO("Error:Wrong gripper status input");
-		return;
-	}
+    if (msg.status == 1) {
+        Gcode = (std::string)"M2232 V1" + "\r\n";
+        ROS_INFO("Closing gripper...");
+    }
+    else if (msg.status == 0) {
+        Gcode = (std::string)"M2232 V0" + "\r\n";
+        ROS_INFO("Opening gripper...");
+    }
+    else {
+        ROS_ERROR("Error: Wrong gripper status input");
+        return;
+    }
 
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Gripper Control Command Completed ===\n");
 }
 
 
@@ -179,15 +221,19 @@ void gripper_callback(const swiftpro::status& msg)
  */
 void pump_callback(const swiftpro::status& msg)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
-	std::string status = std::to_string(msg.status);
+    ROS_INFO("=== Pump Control Callback Called ===");
+    ROS_INFO("Setting pump to: %d (1=on, 0=off)", msg.status);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
+    std::string status = std::to_string(msg.status);
 
-	Gcode = (std::string)"M2231 V" + status + "\r\n";
-
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    Gcode = (std::string)"M2231 V" + status + "\r\n";
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Pump Control Command Completed ===\n");
 }
 
 /*
@@ -197,16 +243,21 @@ void pump_callback(const swiftpro::status& msg)
 */
 void buzzer_callback(const swiftpro::buzzer& buzzer)
 {
-	std::string Gcode = "";
-	std_msgs::String result;
-	std::string time = std::to_string((int(buzzer.time)) * 1000);
-	std::string frequent = std::to_string(buzzer.frequent);
+    ROS_INFO("=== Buzzer Control Callback Called ===");
+    ROS_INFO("Setting buzzer: frequency=%d Hz, time=%d seconds", 
+             buzzer.frequent, buzzer.time);
+    
+    std::string Gcode = "";
+    std_msgs::String result;
+    std::string time = std::to_string((int(buzzer.time)) * 1000);
+    std::string frequent = std::to_string(buzzer.frequent);
 
-	Gcode = (std::string)"M2210 F" + frequent + " T" + time + "\r\n";
-
-	ROS_INFO("%s", Gcode.c_str());
-	_serial.write(Gcode.c_str());
-	result.data = _serial.read(_serial.available());
+    Gcode = (std::string)"M2210 F" + frequent + " T" + time + "\r\n";
+    ROS_INFO("Sending Gcode: %s", Gcode.c_str());
+    _serial.write(Gcode.c_str());
+    result.data = _serial.read(_serial.available());
+    ROS_INFO("Serial response: %s", result.data.c_str());
+    ROS_INFO("=== Buzzer Control Command Completed ===\n");
 }
 
 
