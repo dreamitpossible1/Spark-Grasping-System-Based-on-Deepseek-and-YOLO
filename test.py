a1c67f8eb7fe937364dccfb73d48f40d200b809e
dreamitpossible1 <68002596+dreamitpossible1@users.noreply.github.com>
2025-08-07 17:28:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import rospy
 from sensor_msgs.msg import *
 
-
 def check_grasp_state(data):
     i = 0
     for distance in data.ranges:
@@ -11,8 +10,6 @@ def check_grasp_state(data):
             i += 1
         print("times", i)
         
-
-
 rospy.init_node("check_grasp_state")
 lidar_sub = rospy.Subscriber(
     "/scan", LaserScan, check_grasp_state, queue_size=1, buff_size=2**24
